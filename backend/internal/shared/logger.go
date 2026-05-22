3cb70487232deacd90e74ded8ef6ee0698138bac
Amr Ali <amrrmohamed34342@gmail.com>
2026-05-22 05:42:41 +0300

    refactor: replace logger with structured slog-based logging

--- backend/internal/shared/logger.go
+++ backend/internal/shared/logger.go
@@ -1,46 +1,58 @@
 package shared
 
 import (
-	"fmt"
-	"log"
+	"log/slog"
+	"os"
+	"reflect"
 	"runtime"
 	"strings"
+	"unicode/utf8"
 )
 
-type LogLevel string
+type LogLevel int
 
 const (
-	DEBUG LogLevel = "DEBUG"
-	INFO  LogLevel = "INFO"
-	ERROR LogLevel = "ERROR"
-	WARN  LogLevel = "WARN"
+	DEBUG = iota
+	INFO
+	ERROR
+	WARN
 )
 
-const (
-	infoPrefix  = "[INFO] "
-	warnPrefix  = "[WARN] "
-	errorPrefix = "[ERROR] "
-	debugPrefix = "[DEBUG] "
-)
+type Meta map[string]string
 
-var logger = log.New(log.Writer(), "", log.LstdFlags)
+var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-func Log(level LogLevel, format string, args ...any) {
-	var sfile string = "nil"
-	pointer, file, line, ok := runtime.Caller(1)
+type LogContext struct {
+	File string
+	Line int
+}
+
+func Log(level LogLevel, msg string, meta Meta) {
+	context := LogContext{}
+	_, file, line, ok := runtime.Caller(1)
 	if ok {
-		fn := file[strings.LastIndex(file, "/"):]
-		sfile = fmt.Sprintf("0x%x %s:%d", pointer, fn, line)
+		context.File = file[strings.LastIndex(file, "/"):]
+		context.Line = line
+	}
+
+	var logsData = make([]any, 0, len(meta)*2+reflect.TypeFor[LogContext]().NumField()*2)
+	for k, v := range meta {
+		logsData = append(logsData, k, v)
+	}
+
+	if utf8.RuneCountInString(context.File) != 0 {
+		logsData = append(logsData, "File", context.File)
+		logsData = append(logsData, "Line", context.Line)
 	}
 
 	switch level {
 	case DEBUG:
-		logger.Printf(debugPrefix+sfile+format, args...)
+		logger.Debug(msg, logsData...)
 	case INFO:
-		logger.Printf(infoPrefix+sfile+format, args...)
+		logger.Info(msg, logsData...)
 	case WARN:
-		logger.Printf(warnPrefix+sfile+format, args...)
+		logger.Warn(msg, logsData...)
 	case ERROR:
-		logger.Printf(errorPrefix+sfile+format, args...)
+		logger.Error(msg, logsData...)
 	}
 }
