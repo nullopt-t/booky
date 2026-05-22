3e9a4f2bda7b488efaafbeac0881bc7130776b84
Amr Ali <amrrmohamed34342@gmail.com>
2026-05-22 15:05:45 +0300

    feat: annotate the package for swagger

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,18 @@
+// Run starts the server
+// @title Booky API
+// @version 1.0
+// @description This is the Booky API
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email support@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /api/v1
 package main
 
 import (
+	_ "booky-backend/docs"
 	"booky-backend/internal/app"
 	"context"
 	"errors"
