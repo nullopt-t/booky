9be4ab6bb9b9f32e8f8fe38fca8fc765e253f065
Amr Ali <amrrmohamed34342@gmail.com>
2026-05-27 02:04:34 +0300

    feat: add paginated response function

--- backend/pkg/api/response.go
+++ backend/pkg/api/response.go
@@ -8,8 +8,17 @@ type ErrorResponse struct {
 	Error map[string]string `json:"error"`
 }
 
+type PaginatedResponse[T any] struct {
+	Data T   `json:"data"`
+	Meta any `json:"meta"`
+}
+
 func Success[T any](data T) Response[T] {
 	return Response[T]{Data: data}
+}
+
+func SuccessPaginated[T any](data T, meta any) PaginatedResponse[T] {
+	return PaginatedResponse[T]{Data: data, Meta: meta}
 }
 
 func Error(code, msg string) ErrorResponse {
