e176b1aff333b7db02b3fbc6277f9ddf31dae711
Amr Ali <amrrmohamed34342@gmail.com>
2026-05-30 01:16:52 +0300

    feat: add MessageResponse structure

--- backend/pkg/api/response.go
+++ backend/pkg/api/response.go
@@ -8,6 +8,10 @@ type ErrorResponse struct {
 	Error map[string]string `json:"error"`
 }
 
+type MessageResponse struct {
+	Message string `json:"message"`
+}
+
 type PaginatedResponse[T any] struct {
 	Data T   `json:"data"`
 	Meta any `json:"meta"`
@@ -19,6 +23,12 @@ func Success[T any](data T) Response[T] {
 
 func SuccessPaginated[T any](data T, meta any) PaginatedResponse[T] {
 	return PaginatedResponse[T]{Data: data, Meta: meta}
+}
+
+func SuccessMessage(msg string) MessageResponse {
+	return MessageResponse{
+		Message: msg,
+	}
 }
 
 func Error(code, msg string) ErrorResponse {
