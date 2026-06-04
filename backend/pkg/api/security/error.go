6625b265926ab2153381caaf73f9116a4b6a30fa
Amr Ali <amrrmohamed34342@gmail.com>
2026-06-04 16:50:04 +0300

    feat: add custom api error for security concerns

---
+++ backend/pkg/api/security/error.go
@@ -0,0 +1,36 @@
+package security
+
+import "fmt"
+
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+)
+
+type SecureError struct {
+	Code     string
+	UserMsg  string
+	Internal error
+	MetaData map[string]string
+}
+
+func (se *SecureError) Error() string {
+	return se.UserMsg
+}
+
+func (se *SecureError) LogMessage() string {
+	return fmt.Sprintf(
+		"%s[%s]%s %smsg=%s%s internal=%v meta=%v",
+		Yellow,
+		se.Code,
+		Reset,
+		Green,
+		se.UserMsg,
+		Reset,
+		se.Internal,
+		se.MetaData,
+	)
+}
