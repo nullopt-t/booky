b6168e53979cb7723992e56939951ce81b36a4d9
Amr Ali <amrrmohamed34342@gmail.com>
2026-06-08 18:43:24 +0300

    refactor: move otp functions to separate package

---
+++ backend/pkg/utils/otp.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+func GenerateOTP(length int) (string, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%0*d", length, n.Int64()), nil
+}
