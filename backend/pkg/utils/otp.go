31c7167fb349fbc08f631513d62a69371cadd915
Amr Ali <amrrmohamed34342@gmail.com>
2026-06-11 19:31:02 +0300

    refactor: make the error descriptive

--- backend/pkg/utils/otp.go
+++ backend/pkg/utils/otp.go
@@ -9,7 +9,7 @@ import (
 func GenerateOTP(length int) (string, error) {
 	n, err := rand.Int(rand.Reader, big.NewInt(1000000))
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate OTP: %w", err)
 	}
 	return fmt.Sprintf("%0*d", length, n.Int64()), nil
 }
