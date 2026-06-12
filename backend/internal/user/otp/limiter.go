ca216cfc76e0247cd621892d27573f2829745603
Amr Ali <amrrmohamed34342@gmail.com>
2026-06-13 00:24:58 +0300

    refactor: make the limiter resuable

--- backend/internal/user/otp/limiter.go
+++ backend/internal/user/otp/limiter.go
@@ -2,16 +2,11 @@ package otp
 
 import (
 	"context"
-	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
-)
-
-const (
-	MaxOTPRequests = 5
 )
 
 type Limiter struct {
@@ -22,42 +17,44 @@ func NewRateLimiter(client *redis.Client) *Limiter {
 	return &Limiter{client: client}
 }
 
-func (r *Limiter) AllowOTP(ctx context.Context, userID uuid.UUID) (bool, error) {
-	key := fmt.Sprintf("otp:requests:%s", userID)
-
+func (r *Limiter) Allow(
+	ctx context.Context,
+	key string,
+	limit int64,
+	window time.Duration,
+) (bool, error) {
 	now := time.Now().Unix()
-	window := int64(time.Hour.Seconds())
+	cutoff := now - int64(window.Seconds())
 
-	// Remove requests older than one hour
+	// Remove expired entries
 	if err := r.client.ZRemRangeByScore(
 		ctx,
 		key,
 		"0",
-		strconv.FormatInt(now-window, 10),
+		strconv.FormatInt(cutoff, 10),
 	).Err(); err != nil {
 		return false, err
 	}
 
-	// Count remaining requests
 	count, err := r.client.ZCard(ctx, key).Result()
 	if err != nil {
 		return false, err
 	}
 
-	if count >= MaxOTPRequests {
+	if count >= limit {
 		return false, nil
 	}
 
-	// Store current request
-	err = r.client.ZAdd(ctx, key, redis.Z{
+	if err := r.client.ZAdd(ctx, key, redis.Z{
 		Score:  float64(now),
-		Member: uuid.NewString(),
-	}).Err()
-	if err != nil {
+		Member: uuid.NewString(), // any unique value works
+	}).Err(); err != nil {
 		return false, err
 	}
 
-	// Auto cleanup
-	r.client.Expire(ctx, key, time.Hour)
+	if err := r.client.Expire(ctx, key, window).Err(); err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
